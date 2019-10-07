cf2c5546b461a01feda2bd7d334731886780f18b
LyBanjo <gaultier.jousselin@edu.ece.fr>
2019-10-07 11:25:34 +0200

    Added Basis php + css

---
+++ main.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<link rel="stylesheet" type="text/css" href="main.css">
+	<title>Main Page : CV</title>
+</head>
+<body>
+	<header>
+		<div id="photo" align="center">
+			<img src="img/Kappa.png">	
+			<h1>Jousselin Gaultier</h1>
+		</div>
+	</header>
+	<main>
+			
+
+
+            
+	</main>
+	<footer>
+		<i class="fab fa-linkedin-in">Linkedin</i> <!--FontAwesome a integrer --> 
+		<i class="fab fa-github">Github</i> 
+	</footer>
+</body>
+</html>
